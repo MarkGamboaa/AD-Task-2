e8043aa63be4f0a9090a3fff3f22b75ac635456c
MarkGamboa <202310110@fit.edu.ph>
2025-05-18 13:46:29 +0800

    feat: add menu and best sellers data for improved food item display

---
+++ handlers/menu.handler.php
@@ -0,0 +1,96 @@
+<?php
+$bestSellers = [
+    [
+        "name" => "Adobo",
+        "price" => 149,
+        "desc" => "Classic Filipino dish made with soy sauce and vinegar.",
+        "img" => "adobo.jpg"
+    ],
+    [
+        "name" => "Lechon",
+        "price" => 299,
+        "desc" => "Crispy roasted pork with liver sauce.",
+        "img" => "lechon.jpg"
+    ],
+    [
+        "name" => "Halo-Halo",
+        "price" => 99,
+        "desc" => "Mixed shaved ice dessert with sweet toppings.",
+        "img" => "halohalo.jpg"
+    ]
+];
+
+$menu = [
+    [
+        "name" => "Adobo",
+        "price" => 149,
+        "desc" => "Classic Filipino dish made with soy sauce and vinegar.",
+        "img" => "adobo.jpg"
+    ],
+    [
+        "name" => "Sinigang",
+        "price" => 159,
+        "desc" => "Tamarind-based sour soup with pork and vegetables.",
+        "img" => "sinigang.jpg"
+    ],
+    [
+        "name" => "Lechon",
+        "price" => 299,
+        "desc" => "Crispy roasted pork with liver sauce.",
+        "img" => "lechon.jpg"
+    ],
+    [
+        "name" => "Pancit Bihon",
+        "price" => 129,
+        "desc" => "Stir-fried rice noodles with vegetables and meat.",
+        "img" => "pancit.jpg"
+    ],
+    [
+        "name" => "Kare-Kare",
+        "price" => 189,
+        "desc" => "Oxtail stew with peanut sauce, served with bagoong.",
+        "img" => "karekare.jpg"
+    ],
+    [
+        "name" => "Halo-Halo",
+        "price" => 99,
+        "desc" => "Mixed shaved ice dessert with sweet toppings.",
+        "img" => "halohalo.jpg"
+    ],
+    [
+        "name" => "Laing",
+        "price" => 109,
+        "desc" => "Taro leaves in coconut milk with chili and dried fish.",
+        "img" => "laing.jpg"
+    ],
+    [
+        "name" => "Bistek Tagalog",
+        "price" => 169,
+        "desc" => "Filipino beef steak marinated in soy sauce and calamansi.",
+        "img" => "bistek.jpg"
+    ],
+    [
+        "name" => "Bulalo",
+        "price" => 239,
+        "desc" => "Beef marrow stew with corn and vegetables.",
+        "img" => "bulalo.jpg"
+    ],
+    [
+        "name" => "Tocino",
+        "price" => 129,
+        "desc" => "Sweet cured pork served with garlic rice and egg.",
+        "img" => "tocino.jpg"
+    ],
+    [
+        "name" => "Inihaw na Liempo",
+        "price" => 179,
+        "desc" => "Grilled pork belly marinated in spices.",
+        "img" => "liempo.jpg"
+    ],
+    [
+        "name" => "Ginataang Gulay",
+        "price" => 109,
+        "desc" => "Vegetables in coconut milk with shrimp or dried fish.",
+        "img" => "ginataang.jpg"
+    ]
+];
