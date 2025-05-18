83fcf9663be3a1616b4c934e2e035cb75923bdfa
MarkGamboa <202310110@fit.edu.ph>
2025-05-18 13:46:14 +0800

    feat: add header component to establish page structure and navigation

---
+++ components/templates/header.component.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>(PHP) Pinoy Hot Plates</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>(PHP) Pinoy Hot Plates</h1>
+        <nav>
+            <a href="?page=home">Home</a>
+            <a href="?page=menu">Menu</a>
+        </nav>
+    </header>
+    <main>
