70bfa4f0855c477b949243d652b3f43635a78d53
MarkGamboa <202310110@fit.edu.ph>
2025-05-18 13:45:42 +0800

    feat: update index.php to enhance structure and improve readability

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <?php
 require BASE_PATH . '/vendor/autoload.php';
-require BASE_PATH . '/utils/htmlEscape.utils.php';
-
-require_once BASE_PATH . '/handlers/pageData.handler.php';
-require_once BASE_PATH . '/components/cards/note.component.php';
-require_once BASE_PATH . '/components/templates/foot.component.php';
-require_once BASE_PATH . '/components/templates/head.component.php';
-require_once BASE_PATH . '/components/templates/nav.component.php';
+require BASE_PATH . '/utils/displayMenu.utils.php';
+require_once BASE_PATH . '/handlers/menu.handler.php';
+require_once BASE_PATH . '/components/templates/header.component.php';
 ?>
 
-<?php
-head('Product List');
-navHeader($navList);
-?>
-
-<section class="grid grid-cols-3">
-    <?php displayCardForm($productList); ?>
+<section class="hero">
+    <div class="hero-content">
+        <h1>Welcome to (PHP) Pinoy Hot Plates</h1>
+        <p>Discover mouthwatering dishes made with passion and served with love. Come hungry, leave happy!</p>
+        <a href="?page=menu" class="btn">View Our Menu</a>
+    </div>
 </section>
 
-<?php
-footer();
-?>
+
+<section class="about">
+    <h2>Deliciously Different</h2>
+    <p>At (PHP) Pinoy Hot Plates, we bring people together with comforting flavors, cozy vibes, and unforgettable food.
+        Whether
+        you're in for a quick bite or a full meal, we’ve got something you’ll love.</p>
+</section>
+
+
+<main class="home">
+    <h2>Best Sellers</h2>
+    <div class="menu-list">
+        <?php displayMenu($bestSellers); ?>
+    </div>
+</main>
+
+<?php require_once BASE_PATH . '/components/templates/footer.component.php'; ?>
