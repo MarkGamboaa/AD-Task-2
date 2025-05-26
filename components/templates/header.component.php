8519e4b24a81f5a5c1cfd7cc58007b20b09f3ac5
MarkGamboa <202310110@fit.edu.ph>
2025-05-26 19:28:46 +0800

    fix: update stylesheet and navigation links for correct paths

--- components/templates/header.component.php
+++ components/templates/header.component.php
@@ -3,15 +3,15 @@
 
 <head>
     <title>(PHP) Pinoy Hot Plates</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 
 <body>
     <header>
         <h1>(PHP) Pinoy Hot Plates</h1>
         <nav>
-            <a href="?page=home">Home</a>
-            <a href="?page=menu">Menu</a>
+            <a href="/index.php">Home</a>
+            <a href="/pages/menu/index.php">Menu</a>
         </nav>
     </header>
     <main>
